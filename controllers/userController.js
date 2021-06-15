1d1d0b5b4e88d89560e6d1b94ed1e24ce49686ab
Mohammed Hassad <mohammed.hassad98@gmail.com>
2021-06-15 15:21:41 +0100

    update package.json

---
+++ controllers/userController.js
@@ -0,0 +1,117 @@
+const multer = require('multer');
+const sharp = require('sharp');
+const User = require('./../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+const factory = require('./../controllers/handlerFactory');
+
+const filterObj = (obj, ...allowfields) => {
+  const newObj = {};
+
+  Object.keys(obj).forEach(el => {
+    if (allowfields.includes(el)) newObj[el] = obj[el];
+  });
+
+  return newObj;
+};
+
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user.id;
+  next();
+};
+
+// upload photo
+// const multerStorage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, 'public/img/users');
+//   },
+//   filename: (req, file, cb) => {
+//     const fileName = `user-${req.user._id}-${Date.now()}.${
+//       file.mimetype.split('/')[1]
+//     }`;
+//     cb(null, fileName);
+//   }
+// });
+
+const multerStorage = multer.memoryStorage();
+
+const filter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    return cb(null, true);
+  }
+
+  cb(new AppError('Not an image! Please upload only images.', 400));
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: filter
+});
+
+exports.uploadUserPhoto = upload.single('photo');
+
+exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
+  if (!req.file) return next();
+
+  req.file.filename = `user-${req.user._id}-${Date.now()}.jpeg`;
+
+  await sharp(req.file.buffer)
+    .resize(500, 500)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/users/${req.file.filename}`);
+
+  next();
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1) Create error if user Posts password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password update. Please use /UpdateMyPassword',
+        400
+      )
+    );
+  }
+
+  // 2) Update user documment
+  // req.body.photo = req.file.fileName;
+  const filteredBody = filterObj(req.body, 'name', 'email');
+  if (req.file) filteredBody.photo = req.file.filename;
+
+  const updateUser = await User.findByIdAndUpdate(req.user._id, filteredBody, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updateUser
+    }
+  });
+});
+
+exports.deleteMe = catchAsync(async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user._id, { active: false });
+
+  res.status(204).json({
+    status: 'saccess',
+    data: null
+  });
+});
+
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!'
+  });
+};
+
+exports.getUser = factory.getOne(User);
+exports.getAllUsers = factory.getAll(User);
+
+// Do not updates passwords with this
+exports.updateUser = factory.updateOne(User);
+exports.deleteUser = factory.deleteOne(User);
